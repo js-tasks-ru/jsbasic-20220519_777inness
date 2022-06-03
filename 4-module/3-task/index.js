bf93ebc3d6ceb8bc4bc14f6a04b716fda770990b
Inna Kasyan <777inness@mail.ru>
2022-06-03 15:42:21 +0300

    Lesson 4. DOM

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  
+  for (let i = 1; i < table.rows.length; i++) {
+   
+    if (table.rows[i].cells[3].getAttribute('data-available') == "true") {
+      table.rows[i].classList.add("available")
+    }
+    else if (table.rows[i].cells[3].getAttribute('data-available') == "false") {
+      table.rows[i].classList.add("unavailable")
+    }
+    else if (table.rows[i].cells[3].getAttribute('data-available') == null)
+     table.rows[i].setAttribute("hidden", "true")
+  }
+
+  for (let i = 1; i < table.rows.length; i++) {
+    if (table.rows[i].cells[2].innerHTML == "m") {
+      table.rows[i].classList.add("male")
+    }
+    else if (table.rows[i].cells[2].innerHTML == "f") {
+      table.rows[i].classList.add("female")
+    }
+  }
+
+  for (let i = 1; i < table.rows.length; i++) {
+    if (parseInt(table.rows[i].cells[1].innerHTML, 10)< 18) {
+      table.rows[i].style ="text-decoration: line-through";
+    }
+
+  }
+    return table;
 }
